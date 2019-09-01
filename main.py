2377f7199afde4a438d4660876d8c114aedf71fa
unknown <phantom1470@gmail.com>
2019-08-31 20:45:11 -0700

    Updates LICENSE and README.md and completes task

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Omar Salinas!!!!')
+print('My favorite games include The Legend of Zelda Ocarina of Time and Skyrim')
+print('I am most concerned about learning how to code and how to understand coding in general')
+print('I am most excited to work in a team setting with other students to create video games that actually work!')
+print('My StackedOverflow user number is 12004794')
+print('My GitHub profile URL is https://github.com/SilverSock69')
+print('thank you for viewing my code! I hope to learn how to write python a lot more smoothly and with no stress')
